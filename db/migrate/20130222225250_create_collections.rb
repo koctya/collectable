0bca94ced4bd9bdf4baed1c63b808844ba789ed3
Kurt Landrus <klandrus@gmail.com>
2013-02-22 17:55:08 -0500

    added collection model

---
+++ db/migrate/20130222225250_create_collections.rb
@@ -0,0 +1,13 @@
+class CreateCollections < ActiveRecord::Migration
+  def change
+    create_table :collections do |t|
+      t.string :name
+      t.string :description
+      t.string :type
+      t.references :user
+
+      t.timestamps
+    end
+    add_index :collections, :user_id
+  end
+end
