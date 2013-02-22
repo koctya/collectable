84a3dac5fa95d009636d9a3ea8532565e17a21bd
Kurt Landrus <klandrus@gmail.com>
2013-02-22 16:57:45 -0500

    add user controller, seed for user table

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   def show
   end
   
-  def create
+  def new
   end
   
   def edit
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.create(name: 'Kurt')
+User.create(name: 'Stasya')
+User.create(name: 'Elena')
